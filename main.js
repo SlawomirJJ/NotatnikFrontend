efe2a779bad501c6a674d3a798d16dc28fd32472
SlawomirJJ <81370053+SlawomirJJ@users.noreply.github.com>
2023-10-08 21:43:24 +0200

    Create and delete user added

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ Dodaj użytkownika
         <td>{{dep.gender}}</td>
         <td>
             <button type="button"
-                class="btn btn-light mr-1">
+                class="btn btn-light mr-1"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
                 @click="editClick(dep)">
@@ -43,7 +43,7 @@ Dodaj użytkownika
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                 </svg>
             </button>
-            <button type="button"
+            <button type="button" @click="deleteClick(dep)"
                 class="btn btn-light mr-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
@@ -68,19 +68,19 @@ Dodaj użytkownika
         
     <div class="input-group mb-3">
         <span class="input-group-text">First name</span>
-        <input type="text" class="form-control" v-model="FirstName">
+        <input type="text" class="form-control" v-model="firstName">
     </div>
     <div class="input-group mb-3">
         <span class="input-group-text">Last name</span>
-        <input type="text" class="form-control" v-model="LastName">
+        <input type="text" class="form-control" v-model="lastName">
     </div>
     <div class="input-group mb-3">
         <span class="input-group-text">Birth date</span>
-        <input type="text" class="form-control" v-model="BirthDate">
+        <input type="text" class="form-control" v-model="birthDate">
     </div>
     <div class="input-group mb-3">
         <span class="input-group-text">Gender</span>
-        <input type="text" class="form-control" v-model="Gender">
+        <input type="text" class="form-control" v-model="gender">
     </div>
 
     <button type="button" @click="createClick()"
@@ -97,19 +97,19 @@ data(){
     return{
         users:[],
         modalTitle:"",
-        FirstName:"",
-        LastName:"",
-        BirthDate:"",
-        Gender:""
+        firstName:"",
+        lastName:"",
+        birthDate:"",
+        gender:""
 
 
     };
 },
 mounted() {
-    this.getAllUsers();
+    this.refreshData();
   },
   methods: {
-    async getAllUsers() {
+    async refreshData() {
       try {
         const response = await axios.get('https://localhost:7160/User/GetAllUsers');
         this.users = response.data;
@@ -131,10 +131,57 @@ mounted() {
         this.FirstName=dep.FastName;
         this.LastName=dep.LastName;
     },
-    createClick(){
-        const response = axios.post('https://localhost:7160/User/CreateUser');
-        this.users = response.data;
-    }
+    createClick() {
+        console.log("function createClick is called")
+        
+        const requestData = {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            birthDate: this.birthDate,
+            gender: this.gender
+        };
+    
+        let result = axios.post('https://localhost:7160/User/CreateUser', requestData)
+            .then((result) => {
+                alert(result.data);
+                this.refreshData();
+            });
+
+            console.log("2 function createClick is called", this.firstName,this.lastName,this.birthDate,this.gender)
+    },
+    deleteClick(user) {
+        // Pierwsze, pobieramy ID użytkownika na podstawie jego danych osobowych
+        axios
+          .get(
+            'https://localhost:7160/User/GetUserId',
+            {
+              params: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                birthDate: user.birthDate,
+                gender: user.gender,
+              },
+            }
+          )
+          .then((response) => {
+            const userId = response.data; // Odczytujemy ID użytkownika z odpowiedzi
+      
+            // Następnie usuwamy użytkownika, korzystając z pobranego ID
+            axios
+            .delete(`https://localhost:7160/User/DeleteUser/${userId}`)
+              .then((deleteResponse) => {
+                this.refreshData();
+                alert(deleteResponse.data);
+              })
+              .catch((deleteError) => {
+                console.error('Błąd podczas usuwania użytkownika:', deleteError);
+              });
+          })
+          .catch((error) => {
+            console.error('Błąd podczas pobierania ID użytkownika:', error);
+          });
+      }
+
   },
 
 
